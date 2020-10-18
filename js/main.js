312794bc56465d330d4a67ccfb5741c4d0701440
seiya2341 <73051130+seiya2341@users.noreply.github.com>
2020-10-18 17:55:46 +0900

    initial_new_portfolio

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+{
+  let n = Date.parse('2020/09/04');
+  let m = new Date();
+  let x = (m - n)/1000/60/60/24;
+  x = Math.floor(x);
+
+  const date1 = document.getElementById('date1');
+  date1.textContent = '学習期間: '+ x + '日';
+
+  let a = Date.parse('2020/09/22');
+  let b = new Date();
+  let c = (b - a)/1000/60/60/24;
+  c = Math.floor(c);
+  
+  const date2 = document.getElementById('date2');
+  date2.textContent = '学習期間: ' + c + '日';
+}
